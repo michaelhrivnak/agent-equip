d1d62c7efba5bab8ba9b924881479cccf52bd76f
Michael Hrivnak <mdhrivnak@gmail.com>
2026-07-21 13:12:03 -0400

    removing base laravel files

--- resources/js/components/ui/input-otp/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as InputOTP } from './InputOTP.svelte';
-export { default as InputOTPGroup } from './InputOTPGroup.svelte';
-export { default as InputOTPSeparator } from './InputOTPSeparator.svelte';
-export { default as InputOTPSlot } from './InputOTPSlot.svelte';
